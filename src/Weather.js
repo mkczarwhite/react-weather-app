56f5c1c980fbe8e85b385d7c99254a2187dd1842
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-06-01 19:18:37 -0700

    added search form; added second column

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,10 @@ import './Weather.css';
 
 export default function Weather(){
     return <div className="weather">
+        <form>
+            <input type="search" placeholder="Enter a city..." className="form-control"/>
+            <input type="submit" value="Search" className="btn btn-primary"/>
+        </form>
         <h1>Hilo</h1>
         <ul>
             <li>
@@ -16,6 +20,19 @@ export default function Weather(){
             <div className="col-6">
                 <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly sunny"/>
                 80ºF
+            </div>
+            <div className="col-6">
+                <ul>
+                    <li>
+                        Precipitation: 15%
+                    </li>
+                    <li>
+                        Humidity: 86%
+                    </li>
+                    <li>
+                        Wind: 15mph 
+                    </li>
+                </ul>
             </div>
         </div>
     </div>;
