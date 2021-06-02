acad2752d47e711d8f1b8de93dde8b9456c5cf8d
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-06-01 19:22:04 -0700

    created columns and row to reorganize search

--- src/Weather.js
+++ src/Weather.js
@@ -4,8 +4,16 @@ import './Weather.css';
 export default function Weather(){
     return <div className="weather">
         <form>
-            <input type="search" placeholder="Enter a city..." className="form-control"/>
-            <input type="submit" value="Search" className="btn btn-primary"/>
+            <div className="row">
+                <div className="col-9">
+<input type="search" placeholder="Enter a city..." className="form-control"/>
+                </div>
+<div className="col-3">
+<input type="submit" value="Search" className="btn btn-primary"/>
+</div>
+            
+            </div>
+            
         </form>
         <h1>Hilo</h1>
         <ul>
