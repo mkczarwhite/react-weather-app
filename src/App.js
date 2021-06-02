9e5383114d86e6d26036d470a7f6cab3dffcb07f
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-06-01 18:51:59 -0700

    added link to github repository

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Your travel destiny awaits... </div> 
+  return <div className="App">Your travel destiny awaits... 
+  <p>
+    This project was coded by Michelle White and is available on <a href="https://github.com/mkczarwhite/react-weather-app" target="blank" rel="no-referrer">Github.</a></p></div> 
 }
 
 export default App;
