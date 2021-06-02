9b24a8df951b889b9887a158764f72e2d7e06cc1
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-06-01 19:14:08 -0700

    added border style; updated primary focus for weather

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,21 @@ import React from "react";
 import './Weather.css';
 
 export default function Weather(){
-    return <div className="weather">Hello from weather</div>;
+    return <div className="weather">
+        <h1>Hilo</h1>
+        <ul>
+            <li>
+                Wednesday, 14:30
+            </li>
+            <li>
+                Mostly sunny
+            </li>
+        </ul>
+        <div className="row">
+            <div className="col-6">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly sunny"/>
+                80ºF
+            </div>
+        </div>
+    </div>;
 }
