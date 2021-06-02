af591134c961e4285a8a6960a3817f74dd269643
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-06-01 19:07:12 -0700

    created weather.css, weather component, added to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Weather from './Weather';
 
 function App() {
-  return <div className="App">Your travel destiny awaits... 
+  return <div className="App">
+    <div className="container">
+<Weather />
   <p>
-    This project was coded by Michelle White and is available on <a href="https://github.com/mkczarwhite/react-weather-app" target="blank" rel="no-referrer">Github.</a></p></div> 
+    This project was coded by Michelle White and is available on <a href="https://github.com/mkczarwhite/react-weather-app" target="blank" rel="no-referrer">Github.</a></p>
+    </div>
+    
+    </div> 
 }
 
 export default App;
