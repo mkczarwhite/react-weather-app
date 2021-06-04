2083d94883bcc10d1e86236acbcf2fc814c9b068
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-06-04 08:07:54 -0700

    created tracking for temperature changes

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function WeatherTemperature(props){
+    const [unit, setUnit]=useState("fahrenheit");
+    function convertToCelsius(event){
+        event.preventDefault();
+        setUnit("celsius");
+    }
+    function convertToFahrenheit(event){
+        event.preventDefault();
+        setUnit("fahrenheit");
+    }
+    if (unit==="fahrenheit"){
 return <div className="WeatherTemperature">
-    <span className="temperature">{Math.round(props.data.temperature)}</span><span className="unit">ºF/ºC</span>
+    <span className="temperature">{Math.round(props.fahrenheit)}</span><span className="unit">ºF/<a href="/" onClick={convertToCelsius}>ºC</a></span>
 </div>
+    }else{
+        let celsius=(props.fahrenheit-32)*5/9;
+        return <div className="WeatherTemperature">
+    <span className="temperature">{Math.round(celsius)}</span><span className="unit"><a href="/" onClick={convertToFahrenheit}>ºF</a>/ºC</span>
+</div>
+    }
+
 }
