d38937cdcd1ef6b06c09b6cb46c7cac519f4e886
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-06-07 07:21:45 -0700

    updated css for forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,16 @@
 import React from "react";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast(){
-    return "hello";
+    return (
+        <div className="WeatherForecast">
+            <div className="row">
+                <div className="col">
+                    <div className="WeatherForecast-day">Thu</div> Icon
+                    <div className="WeatherForecast-temperature"><span className="WeatherForecast-temperature-max">83º</span>/<span className="WeatherForecast-temperature-min">74º</span>
+                        </div> 
+                </div>
+            </div>
+        </div>
+    )
 }
