2cf0dbb08a4def3152b3b893c75547f59320f96a
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-06-07 07:52:41 -0700

    built function to run data based on lat/lon & api call

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,17 @@
+import axios from "axios";
 import React from "react";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(){
+    function handleResponse(response){
+        console.log(response.data);
+
+    }
+    let apiKey="4a26ac7808aaa734610b976e82bd1916";
+    let longitude=40.7;
+    let latitude=74;
+    let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
     return (
         <div className="WeatherForecast">
             <div className="row">
