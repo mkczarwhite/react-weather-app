4f0e9957c34f73176589afcfecf463d91b259ef0
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-06-08 07:13:45 -0700

    set up states to load based on data provided

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,26 +1,32 @@
 import axios from "axios";
-import React from "react";
+import React, {useState} from "react";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props){
+    let [loaded, setLoaded]=useState(false);
+    let [forecast, setForecast]=useState(null);
     function handleResponse(response){
-        console.log(response.data);
-
+        setForecast(response.data.daily);
+        setLoaded(true);
     }
-    let apiKey="4a26ac7808aaa734610b976e82bd1916";
+    if (loaded){
+        return (
+        <div className="WeatherForecast">
+            <div className="row">
+                <div className="col">
+        <div className="WeatherForecast-day">{forecast[0].dt}</div> {forecast[0].weather[0].icon}
+        <div className="WeatherForecast-temperature"><span className="WeatherForecast-temperature-max">{forecast[0].temp.max}º</span>/<span className="WeatherForecast-temperature-min">{forecast[0].temp.min}º</span>
+                        </div> 
+                </div>
+            </div>
+        </div>)
+    }
+   else{
+   let apiKey="4a26ac7808aaa734610b976e82bd1916";
     let longitude=props.coordinates.lon;
     let latitude=props.coordinates.lat;
     let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(handleResponse);
-    return (
-        <div className="WeatherForecast">
-            <div className="row">
-                <div className="col">
-                    <div className="WeatherForecast-day">Thu</div> Icon
-                    <div className="WeatherForecast-temperature"><span className="WeatherForecast-temperature-max">83º</span>/<span className="WeatherForecast-temperature-min">74º</span>
-                        </div> 
-                </div>
-            </div>
-        </div>
-    )
+    return null;
+   }
 }
